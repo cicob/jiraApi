a4d4d5c1dd1e3560e6da583d346fddc49627c7a7
Christer Boberg <christer.boberg@gmail.com>
2017-09-21 11:32:46 +0200

    Initial stab

---
+++ src/jira/jiraApi.inc.php
@@ -0,0 +1,589 @@
+<?php
+
+
+
+/********************************************************************************************************************************
+
+The Open Source Initiative --  BSD 2-Clause License
+
+Copyright (c) 2017, Christer Boberg  - GitHub cicob
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+********************************************************************************************************************************/
+
+
+
+
+/**
+ * Jira API Class - for creating Issues
+ * @package    Jira API
+ * @author     Christer Boberg  
+ */
+class jiraApi {
+
+	function __construct($hostname, $jiraUser, $jiraPw, $jiraType) {
+		global $cmdline;
+		$this->cmdline = $cmdline;
+		
+		$this->hostname = $hostname;
+		$this->jiraUser = $jiraUser;
+		$this->jiraPw = $jiraPw;
+		$this->jiraType = $jiraType; // Customer (1), or Default (2)
+		
+		$this->debug = False;
+		$this->result_json = 0;
+		$this->ch = curl_init();
+		$this->component = "";
+		$this->issueType = "Task";
+		$this->projectKey = "";
+		$this->defaultEpic ="";
+		
+		/////////////////////////////
+		// Required Constants
+		/////////////////////////////
+		
+		$this->baseUrl =        $this->hostname . "/rest/api/2/";
+		$this->greenhopperUrl = $this->hostname . "/rest/greenhopper/1.0/";
+		
+	}
+
+
+
+	function addComment($key, $comment){
+		//rest/api/2/issue/{issuekey}/comment
+	
+		$myArray = array(  "body" => $comment);
+		$result = $this->post("issue/".$key."/comment", json_encode($myArray));
+	
+	}
+	
+	
+	function addComponent($key, $component){
+		//rest/api/2/issue/{issuekey}
+		
+		$myArray = array(
+			"fields" => array( 
+					"components" => array(array( "name" => $component ))  
+			) // fields
+		); // myArray
+				
+		$result = $this->put("issue/".$key, json_encode($myArray));
+	
+	}
+	
+
+
+	function addFixVersion($key, $fixVersion){
+		//rest/api/2/issue/{issuekey}
+	
+		$myArray = array(
+				"fields" => array(
+						"fixVersions" => array(array( "name" => $fixVersion ))
+				) // fields
+		); // myArray
+	
+		$result = $this->put("issue/".$key, json_encode($myArray));
+	
+	}
+	
+
+
+	function addLabel($key, $label){
+		//rest/api/2/issue/{issuekey}
+	
+		$myArray = array(
+				"fields" => array(
+						"labels" => array( $label )
+				) // fields
+		); // myArray
+	
+		$result = $this->put("issue/".$key, json_encode($myArray));
+	
+	}
+	
+	
+
+
+
+	/*-
+	 * Close the API-channel. Terminate the communication.
+	 */
+	function close() {
+		curl_close($this->ch);
+	}
+	
+	
+	
+	
+	
+
+	function createIssue($summary, $description, $assignee, $estimate = "0h", $type = "") {
+		
+		
+		if ( $type != "") {
+			$issueType = $type;
+		} else {
+			// Default
+			$issueType = $this->issueType;
+		}
+		
+		$myArray = array( 
+					"fields" => array(  
+						"project" => array( "key" => $this->projectKey),
+						"summary" => $summary,
+						"description" => $description,
+						"issuetype" => array("name" => $issueType),
+						"assignee" => array("name" => $assignee),
+						"timetracking" => array( "originalEstimate" => $estimate)  //,
+						//"components" => array(array( "name" => $this->component ))
+				) // fields
+		); // myArray
+		
+		if ($this->component != "") {
+			$myArray["fields"]["components"] =  array(array( "name" => $this->component ));
+		}
+		
+		
+		
+		
+		// Execute
+		$this->post("issue/", json_encode($myArray));	
+		
+		// Print resulting URL on screen
+		$burl = $this->hostname ."/browse/".$this->resultKey();
+		$this->printStatus($summary.": ", False, True, $burl);
+
+		return $this->resultKey();
+	}
+	
+	
+
+	function createSubTask($summary, $description, $assignee, $estimate = "0h", $type = "", $parent) {
+
+		if ( $type != "") {
+			$issueType = $type;
+		} else {
+			// Default
+			$issueType = "Sub-task"; //default
+		}
+	
+			$myArray = array( 
+							"fields" => array( 
+								"project" => array( "key" => $this->projectKey),
+								"parent"  => array( "key" => $parent),
+								"summary" => $summary,
+								"description" => $description,
+								"issuetype" => array("name" => $issueType),
+								"assignee" => array("name" => $assignee),
+								"timetracking" => array( "originalEstimate" => $estimate)
+					) // fields 
+			); // myArray
+				
+	
+		// Execute
+		$this->post("issue/", json_encode($myArray));
+	
+		// Print resulting URL on screen
+		$burl = $this->hostname ."/browse/".$this->resultKey();
+		$this->printStatus($summary.": ", False, True, $burl);
+		
+		return $this->resultKey();
+	}
+	
+	
+	
+	
+	
+	function linkIssues($inward, $outward, $type = "Blocks", $comment = "" ) {
+		// $type = "Duplicate" or "Blocks" or "Dependency", $comment = "Duplicate issues" etc
+		// The comment goes into the Inward Issue
+		
+		$myArray = array( 
+			"type" => array ("name" => $type),
+			"inwardIssue" => array( "key" => $inward),
+			"outwardIssue" => array( "key" => $outward),
+			"comment" => array( "body" => $comment) 
+		);
+			
+		$this->post("issueLink/", json_encode($myArray));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	// DEPRECATED 
+	private function call($url) {
+		// This implies a HTTP-GET, right?
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($this->ch, CURLOPT_USERPWD,  $this->jiraUser.":".$this->jiraPw  );
+
+		// Status
+		$this->printStatus("Trying call with: ", False, False, $this->baseUrl.$url);
+		curl_setopt($this->ch, CURLOPT_URL, $this->baseUrl.$url);
+		$this->result_json=curl_exec($this->ch);
+		if ($this->result_json === false) {
+			$this->printStatus('Curl error: ' . curl_error($ch), True, True);
+		}
+	}
+	
+	
+	private function httpget($url) {
+		return $this->jiraRest($url, "", "GET");
+	}
+	
+	private function post($url, $json) {
+		return $this->jiraRest($url, $json, "POST");
+	}
+	
+	private function put($url, $json, $myBaseUrl ="") {
+		$result = $this->jiraRest($url, $json, "PUT", $myBaseUrl);
+	}
+	
+	
+	private function jiraRest($url, $json = "", $method, $myBaseUrl = "") {
+	
+		if ($myBaseUrl != "") {
+			$myUrl = $myBaseUrl.$url;
+		} else {
+			$myUrl = $this->baseUrl.$url;  // JIRA API URL
+		}
+		
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($this->ch, CURLOPT_USERPWD,  $this->jiraUser.":".$this->jiraPw  );
+
+		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		if ($json != "") {
+			curl_setopt($this->ch, CURLOPT_POSTFIELDS, $json);
+			curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(
+					'Content-Type: application/json',
+					'Content-Length: ' . strlen($json))
+					);
+		}
+		
+		// Status
+		$this->printStatus("----REST Start------------", False);
+		$this->printStatus("Trying ".$method." with: [".$myUrl."]", False);
+		$this->printStatus("Trying ".$method." with JSON: [".$json."]", False);
+		$this->printStatus("----REST End------------", False);
+		
+		
+		curl_setopt($this->ch, CURLOPT_URL, $myUrl);
+	
+		// Execution - CURL
+		$this->result_json=curl_exec($this->ch);
+
+		// Error-check #1
+		if ($this->result_json === false) {
+			$this->printStatus('Curl ".$method." error: ' . curl_error($this->ch), False, True);
+		}
+		
+		// Error-check #2
+		if (strpos($this->result_json, 'errorMessages') !== false) {
+			$this->printStatus(" ", False, True);
+			$res = json_decode($this->result_json); //Returning a Class
+			foreach($res->errors as $val) {
+				$this->printStatus("**Error** : [".$val."]", False, True);
+			}
+			$this->printStatus(" ", False, True);
+		}
+		
+		$this->printStatus("Return: [".$this->result_json."]", True);
+		
+		return $this->result_json;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	function resultJson() {
+		return $this->result_json;
+	}
+
+
+	function resultClass() {
+		return json_decode($this->result_json); //Returning a Class
+	}
+
+
+	function result() {
+		return json_decode($this->result_json, true); // Returnign an Array
+	}
+
+
+	function resultKey() {
+		$res = json_decode($this->result_json); //Returning a Class
+		return $res->key; // Returning the issue-key
+	}
+
+
+	function resultSummary() {
+		$res = json_decode($this->result_json); //Returning a Class
+		return $res->fields->summary;
+	}
+	
+	
+	
+	//TODO: Not in use yet. Not ready.
+	function newComponent($name, $description, $leadUserName, $assigneeType ="PROJECT_LEAD"){
+	
+		$myArray = array(
+				"name" => $name,
+				"description" => $description,
+				"leadUserName" => $leadUserName,
+				"assigneeType" => $assigneeType,
+				"isAssigneeTypeValid" => "false",
+				"project" => $this->projectKey
+		);
+	
+		$this->post("component/", json_encode($myArray));
+	}
+	
+
+
+
+	
+	function printStatus ($message, $border = False, $force = False, $url = "") {
+		
+		if ($this->debug or $force) {
+			if ($border) {
+				$this->printLine("--------------");
+			}
+
+			// Print text
+			$this->printLine($message);
+
+			// Add URL to output
+			if ($url != "") {
+				
+				
+				if ($this->cmdline) {
+					// Command-line
+					$this->printLine("[ ".$url." ]");
+				} else {
+					//Web-Server
+					$this->printLine(" <a href=\"".$url."\">".$url."</a>");
+				}
+				
+				
+			}
+			
+			
+			if ($border) {
+				$this->printLine("--------------");
+			}
+		} // end debug/force
+	} // end function
+	
+
+
+
+	private function printLine ($message) {
+	
+		print($message);
+
+		if ($this->cmdline) {
+			// Command-line
+			print(PHP_EOL);
+		} else {
+			//Web-Server
+			print("<br>".PHP_EOL);
+		}
+	} // end function
+	
+	
+	
+	
+	
+
+
+	//TODO: Not in use yet. Not ready.
+	function query($issue) {
+		$this->call("issue/". $issue);
+	}
+	
+
+
+	//TODO: Not in use yet. Not ready.
+	function queryLinkIssue($issue) {
+		$this->call("issueLink/". $issue);
+	}
+	
+	
+	
+
+
+	
+
+	/////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////
+	// Getters
+	
+	
+
+	function getDebug() {
+		return $this->debug;
+	}
+	
+	
+
+	/**
+	 * Get type of JIRA-Installation
+	 * (Customer or default out-of-the-box)
+	 * This influences methods for linking tickets.
+	 */
+	function getJiraType() {
+		return $this->jiraType;
+	}
+	
+	
+	
+	
+
+
+	
+	/////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////
+	// Setters
+	
+	
+	
+	
+
+
+	function setComponent($component) {
+		// Set component to use when creating issues
+		$this->component = $component;
+	
+	
+		////////////////////////////////
+		// TODO: Add component if it doesn't exist yet.
+		//    GET /rest/api/2/component/{id}
+	
+		//		$allComponents = $this->httpget("project/".$this->projectKey."/components");
+	
+		//		var_dump(  json_decode(  $allComponents  )   );
+	
+	}
+	
+	
+	
+
+	function setDebug($flag) {
+		$this->debug = $flag;
+	}
+	
+
+	function setDefaultEpic($key) {
+		$this->defaultEpic = $key;
+	}
+	
+	
+	private function setDefaultEpicToIssue($issue) {
+		if ($this->defaultEpic != "") {
+			$this->setEpicToIssue($issue, $this->defaultEpic);
+		}
+	}
+	
+	
+	
+	
+
+
+	function setDueDate($key, $dateString){
+		//rest/api/2/issue/{issuekey} 
+		
+		$myArray = array(
+				"fields" => array(
+						"duedate" => $dateString 
+				) // fields
+		); // myArray
+		
+		$result = $this->put("issue/".$key , json_encode($myArray));
+	}
+	
+	
+
+	function setEpicToIssue($issue, $epicIssue) {
+		$url = "epics/".$epicIssue."/add";
+		$json = '{"ignoreEpics":true,"issueKeys":["'.$issue.'"]}';
+		$result = $this->put($url, $json, $this->greenhopperUrl); // JIRA Greenhopper URL
+		return $result;
+	}
+	
+	
+	
+	
+	/**
+	 * Set new Issue-Type for new issues where it is not specified. The default ist 'Task'
+	 */
+	function setIssueType($issueType) {
+		$this->issueType = $issueType;
+	}
+
+
+	/**
+	 * Set Project Key. Used in all API-calls
+	 */
+	function setProjectKey($projectKey) {
+		$this->projectKey = $projectKey;
+		$this->printStatus("Using JIRA-Project ".$projectKey, False, True, $this->hostname ."/browse/".$this->projectKey);
+	}
+	
+	
+	
+	
+
+
+	function setReporter($key, $reporter){
+		//rest/api/2/issue/{issuekey}/comment
+
+		$myArray = array( "fields" =>
+				array( "reporter" =>
+						array( "name" => $reporter)		
+					) // reporter
+		); // fields
+		
+		$result = $this->put("issue/".$key , json_encode($myArray));
+		
+		
+	}
+	
+	
+	
+
+
+} // end class
+
+?>
---
+++ src/jira/jiraIssue.inc.php
@@ -0,0 +1,202 @@
+<?php
+
+require_once($root."jira/jiraApi.inc.php");
+
+
+
+
+/********************************************************************************************************************************
+
+The Open Source Initiative --  BSD 2-Clause License
+
+Copyright (c) 2017, Christer Boberg  - GitHub cicob
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+********************************************************************************************************************************/
+
+
+
+
+/**
+ * Jira Issues  - Wrapper Class
+ *
+ * @package    Jira API
+ * @author     Christer Boberg 
+ */
+class jiraIssue {
+
+	function __construct($jira, $key="") {
+		$this->jira = $jira;
+		$this->key = $key;
+	}
+
+
+
+	/**
+	 * Set Issue-Key
+	 */
+	function setKey($key) {
+		$this->key = $key;
+	}
+
+
+
+	/**
+	 * Get Issue-Key
+	 */
+	function getKey() {
+		return $this->key;
+	}
+	
+	
+	
+	function create( $summary="No Summary", $description="No Description", $assignee="taaboch5", $estimate="1h", $type = "", $epic = "" ) {
+		
+		$key = $this->jira->createIssue($summary, $description, $assignee, $estimate, $type);
+
+		//Store the key we just created, as a future reference
+		$this->setKey( $key);
+
+		// Set the EPIC if present
+		if ($epic != "") {
+			$this->setEpic($epic);
+		}
+		
+	}
+	
+
+
+
+	function createSub( $summary="No Summary", $description="No Description", $assignee="taaboch5", $estimate="1min", $type = "", $key ) {
+	
+		$keySub = $this->jira->createSubTask($summary, $description, $assignee, $estimate, $type, $key);
+				
+		//Store the key we just created
+		$this->setKey( $keySub);
+	
+	}
+
+
+
+	function dependsOn($firstOne) {
+	
+		switch ($this->jira->getJiraType()) {
+			case "1":
+				// Swisscom JIRA Installation - issue.swisscom.ch
+				//
+				// Sample:
+				// {"id":"10001","name":"Dependency","inward":"is prerequisite for","outward":"depends on","self":"https://issue.swisscom.ch/rest/api/2/issueLinkType/10001"}
+				//
+				$this->jira->LinkIssues( $this->getKey(), $firstOne, "Dependency", "This one can only be done after other linked ticket(s)." );
+				break;
+			case "2":
+				// Default out-of-the-box JIRA isntallation
+				//
+				// Sample
+				// {"id":"10000","name":"Blocks","inward":"is blocked by","outward":"blocks","self":"https://jira.meckecico.com/rest/api/2/issueLinkType/10000"}
+				//
+				$this->jira->LinkIssues( $firstOne, $this->getKey(), "Blocks", "-- This one needs to be done prior to other ticket(s). --" );
+				$this->addComment("This one can only be done after other linked ticket(s).");
+				break;
+			default:
+				//Unknown Target Project
+				$this->jira->printStatus("**Error**  Unknown jiraType: [".$this->jira->getJiraType()."]", True, True);
+		} // end switch
+	} //end Method
+
+
+
+
+
+
+	/**
+	 * Wrapper for API addComment()
+	 */
+	function addComment($message) {
+		$this->jira->addComment( $this->getKey(), $message );
+	}
+
+
+
+	/**
+	 * Wrapper for API addComponent()
+	 * NOTE: The component must exist already
+	 */
+	function addComponent($component){
+		// Current implementation: The component must exist already
+		$this->jira->addComponent($this->getKey(), $component);
+	}
+
+
+
+	/**
+	 * Wrapper for API addFixVersion()
+	 * NOTE: The fixVersion has to exist already
+	 */
+	function addFixVersion($fixVersion){
+		// Current implementation: The fixVersion has to exist already
+		$this->jira->addFixVersion($this->getKey(), $fixVersion);
+	}
+
+
+
+	/**
+	 * Wrapper for API addLabel()
+	 * NOTE: The label has to exist already
+	 */
+	function addLabel($label){
+		// Current implementation: The label has to exist already
+		$this->jira->addLabel($this->getKey(), $label);
+	}
+
+
+
+	/**
+	 * Wrapper for API setEpic()
+	 */
+	function setEpic($epicKey) {
+		$this->jira->setEpicToIssue($this->getKey(), $epicKey);
+	}
+
+
+	/**
+	 * Wrapper for API setDueDate()
+	 */
+	function setDueDate($dateString) {
+		$this->jira->setDueDate($this->getKey(), $dateString);
+	}
+	
+	
+
+	
+	/**
+	 * Wrapper for API setReporter()
+	 */
+	function setReporter($reporter) {
+		$this->jira->setReporter( $this->getKey(), $reporter );
+	}
+	
+	
+	
+	
+	
+	
+} // end class
+
+?>
